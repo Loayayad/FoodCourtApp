7ca04845edc04f261b5c1271516c60e52cc6380c
aya-elsherbiny <ayaahmedelsherbiny@gmail.com>
2021-03-04 19:22:32 +0200

    Starting in menu

---
+++ lib/models/meal/meal.dart
@@ -0,0 +1,12 @@
+class Meal {
+  int id;
+  String name;
+  String image;
+  String discount;
+  String description;
+  int chef;
+  int category;
+  int price;
+  bool show;
+
+Meal(this.id, this.name, this.image, this.discount, this.description, this.chef, this.category, this.price, this.show);}
