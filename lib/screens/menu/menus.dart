ec76548645bd2e07b66f448498ed6dd2a2fadfbc
aya-elsherbiny <ayaahmedelsherbiny@gmail.com>
2021-03-05 12:50:06 +0200

    added meal details and getmealbycatID functionality

--- lib/screens/menu/menus.dart
+++ lib/screens/menu/menus.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:FoodCourtApp/screens/menu/mealDeatails.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../services/menu/menuService.dart';
@@ -12,7 +13,8 @@ class Menu extends StatefulWidget {
   _MenuState createState() => _MenuState();
 }
 
-class _MenuState extends State<Menu> {
+class _MenuState extends State<Menu> with TickerProviderStateMixin{
+  TabController _tabController;
   StreamSubscription<QuerySnapshot> meals;
   List<Meal> mealsList;
   MenuService menuServ = new MenuService();
@@ -20,10 +22,11 @@ class _MenuState extends State<Menu> {
   @override
   void initState(){
     super.initState();
-
+    _tabController = TabController(length:6, vsync: this);
     mealsList = new List();
     meals?.cancel();
-    meals = menuServ.getMealsList().listen((QuerySnapshot snapshot){
+    meals = menuServ.getMealByCategoryID(1).listen((QuerySnapshot snapshot) {
+    //meals = menuServ.getAllMeals().listen((QuerySnapshot snapshot){
       mealsList = snapshot.documents.map((documentSnapshot)
         => Meal(
           documentSnapshot.data['id'],
@@ -44,75 +47,94 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Menu'), backgroundColor: Colors.deepOrange,),
+      appBar: AppBar(title: Text('Menu'), backgroundColor: Colors.deepOrange,
+      bottom:TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                unselectedLabelColor: Colors.white12,
+                tabs: [
+                Tab(child: Text('breakfast')),
+                Tab(child: Text('beef')),
+                Tab(child: Text('chicken')),
+                Tab(child: Text('seafood')),
+                Tab(child: Text('dessert')),
+                Tab(child: Text('sides'))
+              ]),
+      ),
       body: ListView.builder(
-        itemCount: mealsList.length-2,
+        //itemCount: mealsList.length-2,
+        itemCount: mealsList.length,
         itemBuilder: (context, index){
-          return Card(    
-            child: Row(
-              children: [
-                Column(
+          return
+              Card(    
+                child: Row(
                   children: [
-                    Container(
-                      width:120,
-                      height: 100,
-                      child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Image(
-                        image: NetworkImage(mealsList[index].image),
-                      ),
-                      )
-                      // decoration: BoxDecoration(
-                      //   shape: BoxShape.circle,
-                      //   image: DecorationImage(
-                      //     image: NetworkImage(mealsList[index].image),
-                      //     fit: BoxFit.fill,
-                      //   )
-                      // ),
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
+                    Column(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(mealsList[index].name, style: TextStyle(fontSize: 18)),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(mealsList[index].price.toString(), style: TextStyle(color: Colors.deepOrange),),
-                        ),
-                        Text('L.E', style: TextStyle(color: Colors.orange)),
+                        Container(
+                          width:120,
+                          height: 100,
+                          child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image(
+                            image: NetworkImage(mealsList[index].image),
+                          ),
+                          )
+                        
+                        )
                       ],
                     ),
-                    Row(
+                    Column(
                       children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Icon(
-                          Icons.add_shopping_cart_outlined,
-                          color: Colors.orange,
-                          size: 30.0,
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(mealsList[index].name, style: TextStyle(fontSize: 18)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(mealsList[index].price.toString(), style: TextStyle(color: Colors.deepOrange),),
+                            ),
+                            Text('L.E', style: TextStyle(color: Colors.orange)),
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Icon(
-                          Icons.remove_red_eye_outlined,
-                          color: Colors.orange,
-                          size: 30.0,
-                        ),
-                      )
-                    ],)
+                        Row(
+                          children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Icon(
+                              Icons.add_shopping_cart_outlined,
+                              color: Colors.orange,
+                              size: 30.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: GestureDetector(
+                              child:Icon(
+                              Icons.info_outline,
+                              color: Colors.orange,
+                              size: 30.0,
+                            ),
+                            onTap:(){Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => mealDetails(name: mealsList[index].name
+                            ,description: mealsList[index].description
+                            ,price: mealsList[index].price
+                            ,imageLink: mealsList[index].image,
+                            )
+                            )
+                            );}
+                            )
+                          )
+                        ],)
+                      ],
+                    ),
+                    
                   ],
-                ),
-                
-              ],
-              
-            ),        
-          );
+                  
+                ),        
+              );
         })
     );
   }
--- lib/services/menu/menuService.dart
+++ lib/services/menu/menuService.dart
@@ -5,8 +5,15 @@ class MenuService {
   //Read Data From FireStore
   // final CollectionReference meals = Firestore.instance.collection('meals').doc('en').collection('en');
   final CollectionReference meals = Firestore.instance.collection('meals');
-  Stream<QuerySnapshot> getMealsList(){
+  
+  Stream<QuerySnapshot> getMealByCategoryID(catID){
+    // Stream<QuerySnapshot> snapshots = meals.snapshots();
+    Stream<QuerySnapshot> mealsL = Firestore.instance.collection('meals').where('category', isEqualTo: catID).snapshots();
+    return mealsL;
+  }
+  Stream<QuerySnapshot> getAllMeals(){
     Stream<QuerySnapshot> snapshots = meals.snapshots();
     return snapshots;
   }
+
 }
